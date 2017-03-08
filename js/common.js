1420aada594ef14c40513fbc044fe1412ce87a26
karentwan <82635922@qq.com>
2017-03-08 17:57:10 +0800

    前端页面基本完成，修改了搜索栏的样式，将原来现实动画删除

--- js/common.js
+++ js/common.js
@@ -1,16 +1,16 @@
 /**
  * Created by wan on 2017/3/5.
  */
-var search = $("div.nav span.search-btn");
-search.hover(function() {
-    search.hasClass("unfocus") ? search.removeClass("unfocus") : null;
-    search.addClass("focus");
-}, function() {
-    search.removeClass("focus");
-    search.addClass("unfocus");
-    //一秒之后删除unfocus类
-    setTimeout("search.removeClass('unfocus')", 1000);
-});
+// var search = $("div.nav span.search-btn");
+// search.hover(function() {
+//     search.hasClass("unfocus") ? search.removeClass("unfocus") : null;
+//     search.addClass("focus");
+// }, function() {
+//     search.removeClass("focus");
+//     search.addClass("unfocus");
+//     //一秒之后删除unfocus类
+//     setTimeout("search.removeClass('unfocus')", 1000);
+// });
 
 /**
  * 将时间戳转换为日期
